1363a3c37000479cdc3b714de00385a5a96af21b
Utku Berki Baysal <utkuberki21@gmail.com>
2025-08-18 11:31:24 +0300

    @Inject Success

---
+++ src/main/java/com/utkuberki/cdi/_03_Inject.java
@@ -0,0 +1,22 @@
+package com.utkuberki.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.util.List;
+
+@Named(value = "injecttuto")
+@ApplicationScoped
+
+
+public class _03_Inject {
+
+
+    @Inject
+    List<String> consumer;
+
+    public List<String> getConsumer() {
+        return consumer;
+    }
+}
